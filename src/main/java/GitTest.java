6e90d9678ff10017ac92c168cc263de8b0248972
dyc <1910912381@qq.com>
2021-10-19 16:01:41 +0800

    创建新分支

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -6,5 +6,9 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git测试2021年10月19日15:45:48");
+
+        System.out.println("修改代码aaa");
+
+        System.out.println("新分支做修改111");
     }
 }
